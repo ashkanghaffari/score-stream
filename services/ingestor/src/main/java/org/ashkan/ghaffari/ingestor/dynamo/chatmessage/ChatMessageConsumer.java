805351559c636a71da8a2643ea6f0023f8c3668c
Ashkan Ghaffari <ashkanghaffari48@gmail.com>
2025-11-14 14:44:34 -0800

    feat(ChatMessage): Create ChatMessageConsumer for messages in text-clean topic to be persisted in DynamoDB

--- services/ingestor/src/main/java/org/ashkan/ghaffari/ingestor/dynamo/chatmessage/ChatMessageConsumer.java
+++ services/ingestor/src/main/java/org/ashkan/ghaffari/ingestor/dynamo/chatmessage/ChatMessageConsumer.java
@@ -1,4 +1,34 @@
 package org.ashkan.ghaffari.ingestor.dynamo.chatmessage;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.ashkan.ghaffari.ingestor.ws.dto.ChatTextMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.io.IOException;
+
 public class ChatMessageConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(ChatMessageConsumer.class);
+    private static final String CLEAN_TOPIC = "text-clean";
+    private static final String CHAT_MESSAGE_CONSUMER_GROUP_ID = "chat-message-consumer";
+
+    private final KafkaTemplate<String, byte[]> kafka;
+    private final ObjectMapper mapper;
+    private final ChatMessageService chatMessageService;
+
+    public ChatMessageConsumer(KafkaTemplate<String, byte[]> kafka, ObjectMapper mapper,
+                               ChatMessageService chatMessageService) {
+        this.kafka = kafka;
+        this.mapper = mapper;
+        this.chatMessageService = chatMessageService;
+    }
+
+    @KafkaListener(topics = CLEAN_TOPIC, groupId = CHAT_MESSAGE_CONSUMER_GROUP_ID)
+    public void onMessage(byte[] data) throws IOException {
+        ChatTextMessage chatTextMessage = mapper.readValue(data, ChatTextMessage.class);
+        chatMessageService.saveMessage(chatTextMessage);
+    }
 }
