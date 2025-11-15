8f8f30939d8d62894b29ec46952edc2fe515ee36
Ashkan Ghaffari <ashkanghaffari48@gmail.com>
2025-11-15 12:00:56 -0800

    refactor(chat): register kafka consumer bean

--- services/ingestor/src/main/java/org/ashkan/ghaffari/ingestor/dynamo/chatmessage/ChatMessageConsumer.java
+++ services/ingestor/src/main/java/org/ashkan/ghaffari/ingestor/dynamo/chatmessage/ChatMessageConsumer.java
@@ -5,23 +5,21 @@ import org.ashkan.ghaffari.ingestor.ws.dto.ChatTextMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
+@Service
 public class ChatMessageConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(ChatMessageConsumer.class);
     private static final String CLEAN_TOPIC = "text-clean";
     private static final String CHAT_MESSAGE_CONSUMER_GROUP_ID = "chat-message-consumer";
 
-    private final KafkaTemplate<String, byte[]> kafka;
     private final ObjectMapper mapper;
     private final ChatMessageService chatMessageService;
 
-    public ChatMessageConsumer(KafkaTemplate<String, byte[]> kafka, ObjectMapper mapper,
-                               ChatMessageService chatMessageService) {
-        this.kafka = kafka;
+    public ChatMessageConsumer(ObjectMapper mapper, ChatMessageService chatMessageService) {
         this.mapper = mapper;
         this.chatMessageService = chatMessageService;
     }
