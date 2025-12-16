d8a9757003636e35a8757b5c26dfc675290a4055
Ashkan Ghaffari <ashkanghaffari48@gmail.com>
2025-12-15 18:32:58 -0800

    feat(admin-console): scaffold admin service module

---
+++ services/admin-console/src/test/java/org/ashkan/ghaffari/adminconsole/AdminConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ashkan.ghaffari.adminconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
